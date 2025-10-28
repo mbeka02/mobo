d6d318ab9767595d6e135596cb3f8dfd66097c4f
mbeka02 <antonymbeka@gmail.com>
2025-10-28 12:56:35 +0300

    new auth package

---
+++ internal/auth/auth.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"os"
+
+	"github.com/gorilla/sessions"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+var (
+	GOOGLE_CLIENT_ID     = os.Getenv("GOOGLE_CLIENT_ID")
+	GOOGLE_CLIENT_SECRET = os.Getenv("GOOGLE_CLIENT_SECRET")
+	SESSION_SECRET       = os.Getenv("SESSION_SECRET")
+)
+
+func NewAuth() {
+	maxAge := 86400 * 30 // 30 days
+	isProd := false      // Set to true when serving over https
+
+	store := sessions.NewCookieStore([]byte(SESSION_SECRET))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+	goth.UseProviders(
+		google.New(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "http://localhost:3000/auth/google/callback"),
+	)
+}
